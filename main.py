39aaa57d0c4a4ec068133dbc665a0f96a5a131b3
Xuan Cuong <xcuongcs50@gmail.com>
2024-12-21 09:04:52 +0700

    Added test objects

---
+++ main.py
@@ -0,0 +1,6 @@
+# List of Destinations
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+# Test traveler
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
